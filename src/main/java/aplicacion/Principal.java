60bd47e38978a7d779d684208919ff956a968e07
chahla1 <cbouchot@myuax.com>
2025-03-07 17:10:58 +0100

    Commit

--- src/main/java/aplicacion/Principal.java
+++ src/main/java/aplicacion/Principal.java
@@ -1,4 +1,10 @@
 package aplicacion;
 
+import mates.Matematicas;
+
 public class Principal {
+    public static void main(String[] args) {
+        long pasos = 1000000;
+        System.out.println("El número PI es " + Matematicas.generarNumeroPi(pasos));
+    }
 }
