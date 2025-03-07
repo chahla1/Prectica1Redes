36865c41cfb5b282ffa147e9f4e968f33f09cc75
chahla1 <cbouchot@myuax.com>
2025-03-07 17:12:32 +0100

    Commit

--- src/test/java/tests/MatematicasTest.java
+++ src/test/java/tests/MatematicasTest.java
@@ -1,4 +1,14 @@
 package tests;
 
+import mates.Matematicas;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
 public class MatematicasTest {
+    @Test
+    public void testGenerarNumeroPi() {
+        double piAproximado = Matematicas.generarNumeroPi(1000000);
+        assertTrue(piAproximado > 3.1 && piAproximado < 3.2, "El valor de PI debe estar entre 3.1 y 3.2");
+    }
 }
+
